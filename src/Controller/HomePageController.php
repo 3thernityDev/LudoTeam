893caf1f02d025b763519f0ef1c0786479e66315
3thernity <3thernitydev@gmail.com>
2025-01-17 16:22:07 +0400

    [ADD] HomePage

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -2,17 +2,23 @@
 
 namespace App\Controller;
 
+use App\Repository\EventRepository;
+use App\Repository\GameRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 final class HomePageController extends AbstractController
 {
     #[Route('/home/page', name: 'app_home_page')]
-    public function index(): Response
+    public function index(EventRepository $eventRepository, GameRepository $gameRepository): Response
     {
+        $events = $eventRepository->findBy([], ['date' => 'DESC'], 5);
+        $games = $gameRepository->findBy([], [], 5); 
+
         return $this->render('home_page/index.html.twig', [
-            'controller_name' => 'HomePageController',
+            'events' => $events,
+            'games' => $games,
         ]);
     }
 }
