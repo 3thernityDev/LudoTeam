bb421f3277e27a41a46289eb588f92fa9b2f12c9
3thernity <3thernitydev@gmail.com>
2025-01-17 12:01:26 +0400

    [ADD] GameFactory

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\GameFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +10,7 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        GameFactory::new()->createMany(10);
 
         $manager->flush();
     }
