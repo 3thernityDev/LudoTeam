acd90bb98242b64bc884608b36ea756b896d6122
3thernity <3thernitydev@gmail.com>
2025-01-17 11:54:21 +0400

    [ADD] GameController

---
+++ src/Controller/GameController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\GameRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Mapping\Entity;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/game', name: 'app_game')]
+final class GameController extends AbstractController
+{
+    private GameRepository $gameRepository;
+    private EntityManagerInterface $em;
+
+    public function __construct(GameRepository $gameRepository, EntityManagerInterface $em)
+    {
+        $this->gameRepository = $gameRepository;
+        $this->em = $em;
+    }
+
+    #[Route('/', name: '_index')]
+    public function index(): Response
+    {
+        $games = $this->gameRepository->findAll();
+
+
+        return $this->render('game/index.html.twig', [
+            'games' => $games
+        ]);
+    }
+}
